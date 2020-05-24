f549ee47b8171816cec1c0b83679d84b4830808d
caiman <me@caiman.dev>
2020-05-24 21:08:29 +0300

    [master] create Makefile

--- src/tests/test.sh
+++ src/tests/test.sh
@@ -1,12 +1,19 @@
 #! /bin/bash
 
+echo "g++ -std=c++11 -c ../point/Point.cpp"
 g++ -std=c++11 -c ../point/Point.cpp
+echo "g++ -std=c++11 -c ../color/Color.cpp"
 g++ -std=c++11 -c ../color/Color.cpp
+echo "g++ -std=c++11 -c test_point.cpp"
 g++ -std=c++11 -c test_point.cpp
+echo "g++ -std=c++11 -c test_color.cpp"
 g++ -std=c++11 -c test_color.cpp
+echo "g++ -std=c++11 -c main.cpp"
 g++ -std=c++11 -c main.cpp
+echo "build test.out"
 g++ *.o -o test.out
 rm *.o
+echo "Run tests:"
 ./test.out
 # ./test.out -s -r compact
 rm *.out
